9ee34ab04ab1e0f2bb2c30d1c8f7da3234a29a83
RodrigoGuerrero1324 <diego2014717@gmail.com>
2024-07-02 21:46:20 -0500

    primeros-pasos-funcionalidad

---
+++ app.js
@@ -0,0 +1,53 @@
+const encriptar = document.getElementById('encriptar');
+const desencriptar = document.getElementById('desencriptar');
+const copiar =  document.getElementById('copiar');
+const txtmensaje =  document.getElementById('txtMensaje');
+const txtrespuesta = document.getElementById('txtRespuesta');
+
+function activarRespuesta() {
+  // Oculta el primer elemento con la clase 'img_mensaje'
+const imgMensaje = document.querySelector('.contenedor_seccion_mensajes');
+if (imgMensaje) imgMensaje.style.display = 'none';
+
+  // Muestra el primer elemento con la clase 'respuesta_boton'
+const respuestaBoton = document.querySelector('.respuesta_boton_encriptar');
+if (respuestaBoton) respuestaBoton.style.display = 'flex';
+}
+
+function activarImagen(){
+  // Oculta el primer elemento con la clase 'img_mensaje'
+const imgMensaje = document.querySelector('.contenedor_seccion_mensajes');
+if (imgMensaje) imgMensaje.style.display = 'inline';
+
+  // Muestra el primer elemento con la clase 'respuesta_boton'
+const respuestaBoton = document.querySelector('.respuesta_boton_encriptar');
+if (respuestaBoton) respuestaBoton.style.display = 'none';
+}
+
+function onEncriptar() {
+let contenido = txtmensaje.value;
+if(contenido != ''){
+    txtrespuesta.value  = contenido;
+    activarRespuesta();
+}
+}
+
+function onDesencriptar() {
+let contenido = txtmensaje.value;
+if(contenido != ''){
+    txtrespuesta.value  = contenido;
+    activarRespuesta();
+}
+}
+
+function onCopiar(){
+let contenido  = txtmensaje.value;
+if(contenido != ''){
+    activarImagen();
+}
+}
+
+// AGREGANDO EVENTOS A LOS BOTONES.
+encriptar.addEventListener('click', onEncriptar);
+desencriptar.addEventListener('click', onDesencriptar);
+copiar.addEventListener('click',activarImagen);
